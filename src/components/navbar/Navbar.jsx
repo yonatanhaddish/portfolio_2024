d58a57fcbcb0ec3e7bcd0264d99450a181331af1
Yonatan Haddish <yonihailshn@gmail.com>
2024-09-26 12:36:29 -0400

    navbar structured and styled

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./index.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <a href="/">LOGO</a>
+      </div>
+      <div className="links">
+        <ul>
+          <li>
+            <a href="/">HOME</a>
+          </li>
+          <li>
+            <a href="/">ABOUT</a>
+          </li>
+          <li>
+            <a href="/">SERVICE</a>
+          </li>
+          <li>
+            <a href="/">PROJECTS</a>
+          </li>
+          <li>
+            <a href="/">EXPERIENCE</a>
+          </li>
+          <li>
+            <a href="/">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
