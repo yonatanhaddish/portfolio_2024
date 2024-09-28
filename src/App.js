6bd45184eb7294393d818f8c7958d17af061b504
Yonatan Haddish <yonihailshn@gmail.com>
2024-09-28 13:52:22 -0400

    service page initial structured

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./index.css";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
+import Service from "./components/service/Service";
 function App() {
   return (
     <>
       <Navbar />
       <Home />
       <About />
+      <Service />
     </>
   );
 }
---
+++ src/components/service/Service.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./index.css";
+
+function Service() {
+  return (
+    <div className="service-container">
+      <div className='service-heading'></div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Service;
