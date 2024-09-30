551c0c0d7188e307fe11ee04bf3d3a74bec08fa3
Yonatan Haddish <yonihailshn@gmail.com>
2024-09-30 13:07:00 -0400

    project page structured

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -4,12 +4,24 @@ import "./index.css";
 function Project() {
   return (
     <div className="project-container">
-      <div className="project-heading"></div>
+      <div className="project-heading">Projects</div>
       <div className="project-cards-container">
-        <div className="project"></div>
-        <div className="project"></div>
-        <div className="project"></div>
-        <div className="project"></div>
+        <div className="project">
+          <img src="" alt="" className="project-img" />
+          <p className="project-title"></p>
+        </div>
+        <div className="project">
+          <img src="" alt="" className="project-img" />
+          <p className="project-title"></p>
+        </div>
+        <div className="project">
+          <img src="" alt="" className="project-img" />
+          <p className="project-title"></p>
+        </div>
+        <div className="project">
+          <img src="" alt="" className="project-img" />
+          <p className="project-title"></p>
+        </div>
       </div>
     </div>
   );
