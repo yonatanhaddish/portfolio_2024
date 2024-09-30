69b6970cee2da2399d116d4235eb976b22807255
Yonatan Haddish <yonihailshn@gmail.com>
2024-09-30 12:04:52 -0400

    pulling the latest update code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./index.css";
 import Navbar from "./components/navbar/Navbar";
 import About from "./components/about/About";
+import Services from "./components/services/Services";
 function App() {
   return (
     <>
       <Navbar />
       <About />
+      <Services />
     </>
   );
 }
