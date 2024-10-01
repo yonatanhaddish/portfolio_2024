ae0ff0851e1b25ce8d89350b3de31e0556561cce
Yonatan Haddish <yonihailshn@gmail.com>
2024-10-01 17:19:54 -0400

    project page styled

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import "./index.css";
+import alemCoffee from "../../images/alem_coffee.jpeg";
+import alphaTimber from "../../images/alpha_timber.jpeg";
+import summerHouse from "../../images/summer_house.jpeg";
+import zuWeddingDecor from "../../images/zu_wedding_decor.jpeg";
 
 function Project() {
   return (
     <div className="project-container">
-      <div className="project-heading">Projects</div>
+      <div className="project-heading">What I do</div>
       <div className="project-cards-container">
         <div className="project">
-          <img src="" alt="" className="project-img" />
-          <p className="project-title"></p>
+          <img src={alemCoffee} alt="" className="project-img" />
+          <p className="project-title">Alem Cafe</p>
         </div>
         <div className="project">
-          <img src="" alt="" className="project-img" />
-          <p className="project-title"></p>
+          <img src={alphaTimber} alt="" className="project-img" />
+          <p className="project-title">Alpha Timber</p>
         </div>
         <div className="project">
-          <img src="" alt="" className="project-img" />
-          <p className="project-title"></p>
+          <img src={summerHouse} alt="" className="project-img" />
+          <p className="project-title">Summber House</p>
         </div>
         <div className="project">
-          <img src="" alt="" className="project-img" />
-          <p className="project-title"></p>
+          <img src={zuWeddingDecor} alt="" className="project-img" />
+          <p className="project-title">Zu Wedding Decor</p>
         </div>
       </div>
     </div>
