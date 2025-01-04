640c86329a53fb50997a03c4bd4dcec54f2e2232
Yonatan Haddish <yonihailshn@gmail.com>
2025-01-04 11:35:51 -0500

    homepage done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,44 +1,49 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import "./index.css";
 
 function About() {
+  const [currentSelectedImage, setCurrentSelectedImage] = useState("image001");
 
-    const [currentSelectedImage, setCurrentSelectedImage] = useState("image001");
-
-useEffect(() => {
+  useEffect(() => {
     const listImages = ["image001", "image002", "image003"];
     const interval = setInterval(() => {
-        let selectedRandomImage = listImages[Math.floor(Math.random() * listImages.length)]
-        setCurrentSelectedImage(selectedRandomImage)
-    }, 4000)
-    return () => clearInterval(interval)
-}, [])
-  
+      let selectedRandomImage =
+        listImages[Math.floor(Math.random() * listImages.length)];
+      setCurrentSelectedImage(selectedRandomImage);
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className='about-container'>
-        <div className={`about-${currentSelectedImage}`}>
-       </div>
-      <div className='about-description'>
-        <div className='about-desc'>
-            <h3>With a robust foundation in Applied Mathematics and a certificate in Full Stack Development, 
-                my career journey as a Software Developer has been fuled by a passion for coding and problem-solving. 
-                Meticulously leveraging skills in Ansible, React, NodeJs, Express, SQL and NoSQL Databases.
-            </h3>
+    <div className="about-container">
+      <div className={`about-${currentSelectedImage}`}></div>
+      <div className="about-description">
+        <div className="about-desc">
+          <h3>
+            With a robust foundation in Applied Mathematics and a certificate in
+            Full Stack Development, my career journey as a Software Developer
+            has been fuled by a passion for coding and problem-solving.
+            Meticulously leveraging skills in Ansible, React, NodeJs, Express,
+            SQL and NoSQL Databases.
+          </h3>
         </div>
-        <div className='about-desc'>
-            <h3>
-            I am eager to contribute my skills and experience to innovative projects that push the boundaries of technology and 
-            create impact solutions.
-            </h3>
+        <div className="about-desc">
+          <h3>
+            I am eager to contribute my skills and experience to innovative
+            projects that push the boundaries of technology and create impact
+            solutions.
+          </h3>
         </div>
-        <div className='about-desc'>
-            <h3>
-            I am eager to contribute my skills and experience to innovative projects that push the boundaries of technology and create impact solutions.
-            </h3>
+        <div className="about-desc">
+          <h3>
+            I am eager to contribute my skills and experience to innovative
+            projects that push the boundaries of technology and create impact
+            solutions.
+          </h3>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
