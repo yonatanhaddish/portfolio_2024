464bc549e495e209aa68e78a2cc590930762a785
Yonatan Haddish <yonihailshn@gmail.com>
2025-01-11 15:56:47 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -9,22 +9,26 @@ import Skill from "./components/skill/Skill";
 import Experience from "./components/experience/Experience";
 import Contact from "./components/contact/Contact";
 import SideNavbar from "./components/side-navbar/SideNavbar";
+import BoxOne from "./components/BoxOne/BoxOne";
+import BoxTwo from "./components/BoxTwo/BoxTwo";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <SideNavbar
+      <SideNavbar
         pageWrapId={"page-wrap"}
         outerContainerId={"outer-container"}
-      /> */}
-      <Home />
-      <About />
+      />
+      {/* <Home /> */}
+      {/* <About />
       <Service />
       <Project />
       <Skill />
       <Experience />
-      <Contact />
+      <Contact /> */}
+      <BoxOne />
+      <BoxTwo />
     </>
   );
 }
---
+++ src/components/BoxOne/BoxOne.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+function BoxOne() {
+  return <div className="box-one">BoxOne</div>;
+}
+
+export default BoxOne;
