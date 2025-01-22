4d50ce3a875351049c4f983b47c7d733715ec222
Yonatan Haddish <yonihailshn@gmail.com>
2025-01-22 15:49:58 -0500

    mobile size project section done

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <About />
       <Service />
       <Project />
-      {/* <Skill /> */}
+      <Skill />
       {/* <Experience /> */}
       {/* <Contact /> */}
       {/* <BoxOne /> */}
