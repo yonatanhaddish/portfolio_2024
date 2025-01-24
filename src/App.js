9040f689507d5e19da6feeca7ab17d39119584f1
Yonatan Haddish <yonihailshn@gmail.com>
2025-01-24 13:39:23 -0500

    projects section for ipad done

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Home />
       <About />
       <Service />
-      {/* <Project /> */}
+      <Project />
       {/* <Skill /> */}
       {/* <Experience /> */}
       {/* <Contact /> */}
