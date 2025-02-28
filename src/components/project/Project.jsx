01f6364c7a813d348d16e4b439f555fb60a0a113
Yonatan Haddish <yonihailshn@gmail.com>
2025-02-27 22:57:57 -0500

    Modal structured

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -12,10 +12,11 @@ import Typography from "@mui/material/Typography";
 
 const style = {
   position: "absolute",
-  top: "50%",
+  top: "60%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: "80%",
+  height: "40%",
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
